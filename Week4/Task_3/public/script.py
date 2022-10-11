041f545fe0beae79a8518e9cf758b02580f66425
Eevee <evangelinezthoeny@gmail.com>
2022-10-11 22:25:50 +0200

    Exercise 3 solved

--- Week4/Task_3/public/script.py
+++ Week4/Task_3/public/script.py
@@ -6,27 +6,31 @@ import os
 # Do not rename the function or change its list of parameters!
 def absolute_value(a):
     # implement this function
-    pass
+    return abs(a)
 
 # This signature is required for the automated grading to work.
 # Do not rename the function or change its list of parameters!
 def gcd(a, b):
     # implement this function
+    if a < 0:
+        a = absolute_value(a)
+    if b < 0:
+        b = absolute_value(b)
+
     if a == 0 and b == 0:
         return None
     if a == 0 or b == 0:
         return a if b == 0 else b
     if a < b:
-        return gcd(a - a, b - a)
+        return gcd(a, b % a)
     else:
-        return gcd(a - b, b - b)
-
+        return gcd(a % b, b )
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
 # can easily test many different values on every "Test & Run"!
-a = 33
-b = 17
+a = 1000000000
+b = 1
 print(f"greatest common divisor of {a} and {b} is = {gcd(a, b)}")
 
