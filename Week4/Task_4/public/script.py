5b6163053bc437ef0cd028fd465da5f3adb224df
Eevee <evangelinezthoeny@gmail.com>
2022-10-12 15:07:20 +0200

    solved exercise 4, week 4 dun les goo

--- Week4/Task_4/public/script.py
+++ Week4/Task_4/public/script.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # These are examples for valid and invalid inputs to your validation function
-IPv4_STRING = "127.0.0.1"
+IPv4_STRING = "127.0.0.0"
 IPv4_INVALID_STRING = "300.0.0.1"
 IPv6_STRING = "2001:0db8:85a3:0000:0000:8a2e:0370:7334"
 IPv6_INVALID_STRING = "2001:0db8:85a3:0000:0000:8a2e:0370:7334:7334"
@@ -10,40 +10,32 @@ INVALID_IP_STRING = "Some arbitrary string"
 
 def is_valid_IPv4_octet(octet: str):
     """Returns True if octet represents a valid IPv4 octet, False otherwise"""
-    pass
+    return False if int(octet) < 0 or int(octet) > 255 else True
 
 def is_valid_IPv4(ip: str):
     """Returns True if ip represents a valid IPv4 address, False otherwise"""
-    ip = ip.split('.')
     for i in range(4):
-        if int(ip[i]) < 0 or int(ip[i]) > 255:
-            print(ip[i])
+        if not (is_valid_IPv4_octet(int(ip.split('.')[i])) and (len(ip.split('.')) == 4)):
             return False
     return True
 
 def is_valid_IPv6_hextet(hextet: str):
     """Returns True if hextet represents a valid IPv6 hextet, False otherwise"""
-    pass
+    return False if (not hextet.isdigit()) and (int(hextet, 16) < 0 or int(hextet, 16) > int('FFFF', 16)) else True
 
 def is_valid_IPv6(ip: str):
     """Returns True if ip represents a valid IPv6 address, False otherwise"""
-    ip = ip.split(':')
     for i in range(8):
-        if (not ip[i].isdigit()) and (int(ip[i], 16) < 0 or int(ip[i], 16) > int('FFFF', 16)):
-            print(ip[i])
+        if not (is_valid_IPv6_hextet(ip.split(':')[i]) and (len(ip.split(':')) == 8)):
             return False
     return True
 
 def is_valid_IP(ip: str):
     """Returns True if ip represents a valid IPv4 or IPv6 address False otherwise"""
     if '.' in ip:
-        if len(ip.split('.')) == 4:
-            if is_valid_IPv4(ip):
-                return True
+            return is_valid_IPv4(ip)
     elif ':' in ip:
-        if len(ip.split(':')) == 8:
-            if is_valid_IPv6(ip):
-                return True
+            return is_valid_IPv6(ip)
     return False
 
 
