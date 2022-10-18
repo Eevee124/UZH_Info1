0bd056ad49c10bdc7d1aad7edf8c7fc647eb36aa
Eevee <evangelinezthoeny@gmail.com>
2022-10-18 17:31:28 +0200

    ex 1

--- Week5/Task_1/public/script.py
+++ Week5/Task_1/public/script.py
@@ -4,13 +4,34 @@
 # Do not rename the function or change its list of parameters!
 
 def merge(a, b):
-    mergelist = []
-    return mergelist
 
-#sdf
+    if a == [] or b == []:
+        return []
+
+    if len(a) < len(b):
+        lengthen(a, len(b))
+
+    elif len(b) < len(a):
+        lengthen(b, len(a))
+
+    mergelist = []
+
+    print(len(a), " ", len(b))
+
+    for i in range(len(a)):
+        mergelist.append((a[i], b[i]))
+    
+    print(mergelist)
+
+    return list(zip(a, b))
+
+def lengthen(a, length):
+    while len(a) < length:
+        a.append(a[-1])
+    return a
 
 # The following line calls the function and prints the return
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
 # can easily test many different values on every "Test & Run"!
-print(merge([0, 1, 2], [5, 6]))
+print(merge([1, 1, 6, 8, 9], [2, 3, 4]))
