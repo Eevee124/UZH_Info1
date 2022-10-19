54f802a38d3ad17722de3daba7393c6cbc7693ed
Eevee <evangelinezthoeny@gmail.com>
2022-10-19 13:52:18 +0200

    Task 5 done

--- Week5/Task_2/public/script.py
+++ Week5/Task_2/public/script.py
@@ -53,4 +53,4 @@ def no_dupes(a, b):
 # value to the Console. This way you can check what it does.
 # However, we encourage you to write tests, because then you
 # can easily test many different values on every "Test & Run"!
-print(invert({}))
+print(invert({"a":1, "b":1, "c":3}))
--- Week5/Task_5/public/script.py
+++ Week5/Task_5/public/script.py
@@ -6,43 +6,37 @@
 # Do not rename the function or change its list of parameters!
 
 def hamming_dist(signal_1, signal_2):
-    if signal_1 == {} and signal_2 == ():
-        return "Empty signal on at least one of the sensors"
 
     signal_1 = signal_1["data"]
-    signal_2 = list(signal_2)
+    signal_2 = list(signal_2) #technically unnecessary but will cause AttributeError because of empty check
 
     if len(signal_1) != len(signal_2) or (signal_1 == [] and signal_2 == []):
         return "Empty signal on at least one of the sensors"
+    #Check length equality
 
-    first = [len(item) for item in signal_1]
-    second = [len(item) for item in signal_2]
-    lengths = [pair[0] - pair[1] for pair in zip(first, second)]
-
-    if sum(lengths) != 0:
+    if any (len(first) != len(second) for first, second in zip(signal_1, signal_2)):
         return "Sensor defect detected"
+    #Check string length equality
 
     ham = [hamming(pear[0], pear[1]) for pear in zip(signal_1, signal_2)]
+    #create list of hamming distances of tuples where you compare first element with second in tuple
 
-    for i in range(len(ham)-1, -1, -1):
-        if ham[i] == 0:
-            signal_1.pop(i)
-            signal_2.pop(i)
-            ham.pop(i)
-
-    return list(zip(signal_1, signal_2, ham))
+    return [(first, second, dist) for first, second, dist in (zip(signal_1, signal_2, ham)) if dist]
+    #return triplet tuple of signal 1, 2 and hamming distance whenever hamming distance is not equal to 0
 
 def hamming(a, b):
-    dist = 0
+    #Function that computes hamming distance between two strings
+    distance = 0
     for i in range(len(a)):
         if a[i] != b[i]:
-            dist += 1
-    return dist
+            distance += 1
+    return distance
 
 # The following lines print your function's output for an exemplary input to the console.
 # Note that this does not include any of the mentioned edge cases for defective sensors or signals of different lenghts.
 # Try to write your own tests for this.
 
-signal_sensor_1 = {}
-signal_sensor_2 = ()
+signal_sensor_1 = {"times": [0, 1, 2, 3, 4, 5],
+                   "data": ["00101110", "11001011", "11110000", "01000011", "11001101", "00011011"]}
+signal_sensor_2 = ("00101110", "11001001", "11110011", "01111011", "11001101", "00011011")
 print(hamming_dist(signal_sensor_1, signal_sensor_2))
